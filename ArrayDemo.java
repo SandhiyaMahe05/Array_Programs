a8e6f7d142e37f5041379617a5b72095c5638480
SandhiyaMahe05 <98269794+SandhiyaMahe05@users.noreply.github.com>
2022-04-07 20:48:51 +0530

    Update ArrayDemo.java

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -6,7 +6,6 @@ public class ArrayDemo {
 	public static void main(String[] args) {
 		ArrayDemo ad = new ArrayDemo();
 		ad.printArray();
-
 		ad.printArray2();
 		ad.printArray3();
 		}
@@ -14,23 +13,25 @@ private void printArray3() {
 		// TODO Auto-generated method stub
 		int[] marks = { 93, 94, 95, 99, 99 };
 		int total = 0;
-
+System.out.println();
 		for (int i = 4; i >= 0; i--) {
 			// if (marks[i] % 2 == 0)
 			total = total + marks[i];
 			// System.out.println(marks[i]);
 		}
-		System.out.println(total);
-		System.out.println(total / 5 + "%");
+		System.out.println("Total mark: "+total);
+		System.out.println("Percentage: "+total / 5 + "%");
 	}
 
 	private void printArray2() {
 		// TODO Auto-generated method stub
 		int[] marks = { 93, 94, 95, 99, 99 };
-
+		System.out.println("Descending order");
+        
 		for (int i = 4; i >= 0; i--)
 		// for(int i =1;i<5;i+=2){
 		{
+		
 			System.out.println(marks[i]);
 
 		}
@@ -39,6 +40,7 @@ private void printArray3() {
 	private void printArray() {
 		// TODO Auto-generated method stub
 		int[] marks = { 93, 94, 95, 99, 99 };
+		System.out.println("Printing the marks");
 
 		for (int i = 0; i < 5; i++) {
 			System.out.println(marks[i]);
