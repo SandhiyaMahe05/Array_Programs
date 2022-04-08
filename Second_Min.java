9d3dfd8a4591e598614552e9050ad2acc6794ccb
SandhiyaMahe05 <98269794+SandhiyaMahe05@users.noreply.github.com>
2022-04-08 17:12:29 +0530

    Add files via upload

---
+++ Second_Min.java
@@ -0,0 +1,30 @@
+package program_arrays;
+
+import java.util.Scanner;
+
+public class Second_Min {
+	public static void main(String[] args) {
+		Second_Min smin = new Second_Min();
+	 smin.sec_Min();
+	}
+
+	private void sec_Min() {
+		// TODO Auto-generated method stub
+		int[] marks = { 98, 99, 67, 85, 66 };
+		int min = marks[0];
+		int sec_min = 0;
+		int n;
+		for (n = 1; n < marks.length; n++) {
+			if (min > marks[n]) {
+				sec_min = min;
+				min = marks[n];
+			} else if (marks[n] < sec_min) {
+				sec_min = marks[n];
+			}
+
+		}
+		System.out.println("Minumum mark is: " + min);
+		System.out.println("Second minimum mark is: " + sec_min);
+
+	}
+}
