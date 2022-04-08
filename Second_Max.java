c056a7fb48746e68a80d04510f7fda3f8c0589c4
SandhiyaMahe05 <98269794+SandhiyaMahe05@users.noreply.github.com>
2022-04-08 17:07:28 +0530

    Add files via upload

---
+++ Second_Max.java
@@ -0,0 +1,29 @@
+package program_arrays;
+
+import java.util.Scanner;
+
+public class Second_Max {
+	public static void main(String[] args) {
+		Second_Max sm = new Second_Max();
+		sm.sec_Max();
+	}
+private void sec_Max() {
+		// TODO Auto-generated method stub
+		int[] marks = { 99, 95, 98, 97, 94 };
+		int max = 0;
+		int sec_Max = 0;
+		int i;
+		for (i = 0; i < marks.length; i++) {
+			if (marks[i] > max) {
+				sec_Max = max;
+				max = marks[i];
+			} else if (marks[i] > sec_Max)
+			{
+				sec_Max = marks[i];
+			}
+		}
+		System.out.println("Maximum mark is: "+max);
+		System.out.println("Second Maximum mark is: "+sec_Max);
+
+	}
+	}
